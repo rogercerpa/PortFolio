23768f0c9ef97964d5ba0a9b77fa1654552f7f06
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-24 22:55:47 -0400

    Updates

---
+++ precache-manifest.edb76e7998482598db3064bf1534449d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ca2d4fca5fad61b4705c5a7402a3b2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0bde621a3c0affe7d404",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afda21390b6b4cb282a8",
+    "url": "/react-gh-pages/static/js/2.262eeec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.262eeec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bde621a3c0affe7d404",
+    "url": "/react-gh-pages/static/js/main.6634911e.chunk.js"
+  },
+  {
+    "revision": "706eaf771645f320f180",
+    "url": "/react-gh-pages/static/js/runtime-main.20128ff0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);

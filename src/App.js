36898d2b320029c82f3b0cf745a5291f2384adef
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-03 13:57:20 -0400

    added routers for the multiple pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/navbar/index';
+import Footer from './components/footer/index';
+import AboutMe from './pages/aboutMe';
+import Projects from './pages/projects';
+import Contact from './pages/contact';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-	return <div className="App App-header">Hello Super Roger</div>;
+	return (
+		<Router>
+			<div className="container">
+				<NavBar />
+				<Switch>
+					<Route exact path={[ '/', '/aboutme' ]}>
+						<AboutMe />
+					</Route>
+					<Route exact path={[ '/projects' ]}>
+						<Projects />
+					</Route>
+					<Route exact path={[ '/contact' ]}>
+						<Contact />
+					</Route>
+				</Switch>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/footer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function StickyFooter() {
+	return <div>Super Roger</div>;
+}
+
+export default StickyFooter;

91ce179870dc180baea0f59523a99f54ae82842b
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-27 23:03:43 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c0f16b50feffb73c23268212c0521500.js"
+  "/portfolio/precache-manifest.a9d7ff6d9f67d33f20ad3f138e6f8cfd.js"
 );
 
 self.addEventListener('message', (event) => {

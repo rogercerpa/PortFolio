e9861357c35aecc092277c678490b0024b860f0a
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-27 23:02:50 -0400

    updated the homepage content, added profile image

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -4,7 +4,8 @@ import { Jumbotron, Container } from 'reactstrap';
 const AboutMe = () => {
 	return (
 		<Jumbotron fluid>
-			<Container fluid>
+			<Container className="themed-container">
+				<img src="../../Assets/images/IMG_3602.jpg" alt="rogerPicture" />
 				<h1 className="display-3">Fluid jumbotron</h1>
 				<p className="lead">
 					This is a modified jumbotron that occupies the entire horizontal space
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,14 +1,86 @@
-import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+	Carousel,
+	CarouselItem,
+	CarouselControl,
+	CarouselIndicators,
+	CarouselCaption
+} from 'reactstrap';
 
-const HomePage = () => {
+const items = [
+	{
+		src     : '../../public/Assets/images/liveSearch-demo-image.png',
+		altText : 'Slide 1',
+		caption : 'Slide 1'
+	},
+	{
+		src     : '../public/Assets/images/liveSearch-demo-image.png',
+		altText : 'Slide 2',
+		caption : 'Slide 2'
+	},
+	{
+		src     : '../public/Assets/images/liveSearch-demo-image.png',
+		altText : 'Slide 3',
+		caption : 'Slide 3'
+	}
+];
+
+const HomePage = (props) => {
+	const [ activeIndex, setActiveIndex ] = useState(0);
+	const [ animating, setAnimating ] = useState(false);
+
+	const next = () => {
+		if (animating) return;
+		const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+		setActiveIndex(nextIndex);
+	};
+
+	const previous = () => {
+		if (animating) return;
+		const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+		setActiveIndex(nextIndex);
+	};
+
+	const goToIndex = (newIndex) => {
+		if (animating) return;
+		setActiveIndex(newIndex);
+	};
+
+	const slides = items.map((item) => {
+		return (
+			<CarouselItem
+				onExiting={() => setAnimating(true)}
+				onExited={() => setAnimating(false)}
+				key={item.src}
+			>
+				<img src={item.src} alt={item.altText} />
+				<CarouselCaption
+					captionText={item.caption}
+					captionHeader={item.caption}
+				/>
+			</CarouselItem>
+		);
+	});
+
 	return (
-		<Jumbotron fluid>
-			<Container fluid>
-				<h1 className="display-3">Roger Cerpa Coding Life</h1>
-				<p className="lead">Find out more...</p>
-			</Container>
-		</Jumbotron>
+		<Carousel activeIndex={activeIndex} next={next} previous={previous}>
+			<CarouselIndicators
+				items={items}
+				activeIndex={activeIndex}
+				onClickHandler={goToIndex}
+			/>
+			{slides}
+			<CarouselControl
+				direction="prev"
+				directionText="Previous"
+				onClickHandler={previous}
+			/>
+			<CarouselControl
+				direction="next"
+				directionText="Next"
+				onClickHandler={next}
+			/>
+		</Carousel>
 	);
 };
 

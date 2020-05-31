5b98951cedc892d0ee604266478e04607c27cf89
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-30 23:02:36 -0400

    added project image, started project list styling

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import './Assets/style/projects.css';
 
 export default function MediaCard() {
 	return (
 		<div>
 			<Container>
 				<Row>
-					<Col sm="12" md={{ size: 6, offset: 3 }}>
-						Hello to Rogers best projects
+					<Col sm={{ size: 6, order: 2, offset: 1 }}>
+						<h1 className="projectList"> Projects </h1>
+					</Col>
+				</Row>
+				<Row xs="2">
+					<Col>
+						<img
+							alt="project1"
+							src="./Assets/images/liveSearch-demo-image.png"
+							height="200"
+							width="200"
+						/>
+					</Col>
+					<Col>
+						<div>
+							<h2 className="projectTitle">liveSearch-App</h2>
+						</div>
 					</Col>
 				</Row>
 			</Container>

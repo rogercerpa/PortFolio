78eef60c9e4104aff1c9dbfb46a3bc000dd4946c
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-06-16 20:27:32 -0400

    rename images and added images to the home page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -8,27 +8,35 @@ import {
 	Container
 } from 'reactstrap';
 import './Assets/style/style.css';
-import RogerImage1 from './Assets/images/roger1.jpg';
+import RogerExplorer from './Assets/images/rogerExplorer.jpg';
+import RogerMTB from './Assets/images/rogerMTB.jpg';
+import RogerPhotographer from './Assets/images/rogerPhotographer.JPG';
 
 const items = [
 	{
-		src     : RogerImage1,
-		altText : 'Slide 1',
-		caption : 'Slide 1',
-		width   : 200,
-		height  : 200
+		src      : RogerExplorer,
+		altText  : 'Roger1',
+		caption  : 'Roger',
+		caption2 : 'The Explorer'
 	},
 	{
-		src     :
-			'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-		altText : 'Slide 2',
-		caption : 'Slide 2'
+		src      : RogerMTB,
+		altText  : 'Roger2',
+		caption  : 'Roger',
+		caption2 : 'The Mountain Biker'
 	},
 	{
-		src     :
+		src      :
 			'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-		altText : 'Slide 3',
-		caption : 'Slide 3'
+		altText  : 'Roger3',
+		caption  : 'Roger',
+		caption2 : 'The Programmer'
+	},
+	{
+		src      : RogerPhotographer,
+		altText  : 'Roger4',
+		caption  : 'Roger',
+		caption2 : 'The Photographer'
 	}
 ];
 
@@ -60,9 +68,9 @@ const HomePage = (props) => {
 				onExited={() => setAnimating(false)}
 				key={item.src}
 			>
-				<img src={item.src} alt={item.altText} />
+				<img src={item.src} alt={item.altText} width="1100" height="600" />
 				<CarouselCaption
-					captionText={item.caption}
+					captionText={item.caption2}
 					captionHeader={item.caption}
 				/>
 			</CarouselItem>

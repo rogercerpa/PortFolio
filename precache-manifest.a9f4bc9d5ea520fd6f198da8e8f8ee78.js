8ffa4b296862b2d8560a0d5ec5b8411334e1ab89
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-06-16 21:38:28 -0400

    Updates

---
+++ precache-manifest.a9f4bc9d5ea520fd6f198da8e8f8ee78.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0fd55cd302dc7d9c9692b02d4fc6a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7fdd1c68d0d3277fe81e",
+    "url": "/portfolio/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "cf5709888ba9137a6c33",
+    "url": "/portfolio/static/css/main.c00654f2.chunk.css"
+  },
+  {
+    "revision": "7fdd1c68d0d3277fe81e",
+    "url": "/portfolio/static/js/2.44e45c0b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.44e45c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5709888ba9137a6c33",
+    "url": "/portfolio/static/js/main.0538ce14.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "3e525e84c7e32c1bca91d8cbb9d82e21",
+    "url": "/portfolio/static/media/liveLy.3e525e84.PNG"
+  },
+  {
+    "revision": "6b5ae9a315a8d2f12c2bcd6baa820e1b",
+    "url": "/portfolio/static/media/liveSearch.6b5ae9a3.png"
+  },
+  {
+    "revision": "585622159f76bdc8a6b5e62d4a4ab55f",
+    "url": "/portfolio/static/media/rogerExplorer.58562215.jpg"
+  },
+  {
+    "revision": "084f30790e5baea8d0bb7bc95b2d64cf",
+    "url": "/portfolio/static/media/rogerMTB.084f3079.jpg"
+  },
+  {
+    "revision": "587b0c2800101a1d5a4dfc5beef2f364",
+    "url": "/portfolio/static/media/rogerPhotographer.587b0c28.JPG"
+  },
+  {
+    "revision": "887a364bb33772dbd28e0c18f8029aee",
+    "url": "/portfolio/static/media/rogerProgrammer.887a364b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ff59f9e9193f684c20918e46f98d71fb.js"
+  "/portfolio/precache-manifest.a9f4bc9d5ea520fd6f198da8e8f8ee78.js"
 );
 
 self.addEventListener('message', (event) => {

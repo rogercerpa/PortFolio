f1fd065ffcc14a32589ba652a94646a95d6876c0
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-06-16 20:48:42 -0400

    added programmer image, updated homepage

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Jumbotron, Container } from 'reactstrap';
-import rogerImage from './Assets/images/roger1.jpg';
+import rogerExplorer from './Assets/images/rogerExplorer.jpg';
 
 const AboutMe = () => {
 	return (
 		<Jumbotron fluid>
 			<Container className="themed-container">
-				<img src={rogerImage} alt="rogerPicture" width="500" height="333" />
+				<img src={rogerExplorer} alt="rogerPicture" width="500" height="333" />
 				<h1 className="display-3">Roger Cerpa</h1>
 				<p className="lead">
 					This is a modified jumbotron that occupies the entire horizontal space
